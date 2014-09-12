8f3867724b462183565c10642daecb4a36fa7c11
taekout <taekout@gmail.com>
2014-09-12 12:52:14 -0400

    Comment out some debugging stuff.

--- VolumeRenderer/RenderEngine.cpp
+++ VolumeRenderer/RenderEngine.cpp
@@ -23,7 +23,7 @@ void RenderScene()
 
 	//gRenderEngine->SetCamera(gLightPos, gLightDir);
 	//gRenderEngine->SetupRenderTarget();
-	gRenderEngine->fCamera->GetEyeDir();
+	//gRenderEngine->fCamera->GetEyeDir();
 	printOpenGLError();
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	printOpenGLError();
